776af7a44d72e950a0f95b3cfb9d0442d0f5b916
Matthias Balke <Matthias.Balke@googlemail.com>
2026-03-28 21:37:07 +0100

    fix linting/shell check errors

--- todo/rootfs/start.sh
+++ todo/rootfs/start.sh
@@ -5,54 +5,64 @@
 
 # get HA addon config
 if bashio::config.has_value 'springDatasourceUrl'; then
-    export SPRING_DATASOURCE_URL=$(bashio::config 'springDatasourceUrl')
+    SPRING_DATASOURCE_URL=$(bashio::config 'springDatasourceUrl')
+    export SPRING_DATASOURCE_URL
 else
     bashio::log.error "\"springDatasourceUrl\" not set. Please set \"springDatasourceUrl\" in add-on config. Aborting."
     exit 1;
 fi
 
 if bashio::config.has_value 'springDatasourceUsername'; then
-    export SPRING_DATASOURCE_USERNAME=$(bashio::config 'springDatasourceUsername')
+    SPRING_DATASOURCE_USERNAME=$(bashio::config 'springDatasourceUsername')
+    export SPRING_DATASOURCE_USERNAME
 else
     bashio::log.error "\"springDatasourceUsername\" not set. Please set \"springDatasourceUsername\" in add-on config. Aborting."
     exit 1;
 fi
 if bashio::config.has_value 'springDatasourcePassword'; then
-   export SPRING_DATASOURCE_PASSWORD=$(bashio::config 'springDatasourcePassword')
+   SPRING_DATASOURCE_PASSWORD=$(bashio::config 'springDatasourcePassword')
+   export SPRING_DATASOURCE_PASSWORD
 else
     bashio::log.error "\"springDatasourcePassword\" not set. Please set \"springDatasourcePassword\" in add-on config. Aborting."
     exit 1;
 fi
 
 if bashio::config.has_value 'jwtSecret'; then
-   export JWT_SECRET=$(bashio::config 'jwtSecret')
+   JWT_SECRET=$(bashio::config 'jwtSecret')
+   export JWT_SECRET
 else
     bashio::log.error "\"jwtSecret\" not set. Please set \"jwtSecret\" in add-on config. Aborting."
     exit 1;
 fi
 
 if bashio::config.has_value 'origin'; then
-   export ORIGIN=$(bashio::config 'origin')
-   export CORS_ALLOWED_ORIGINS=$(bashio::config 'origin')
+   ORIGIN=$(bashio::config 'origin')
+   export ORIGIN
+   CORS_ALLOWED_ORIGINS=$(bashio::config 'origin')
+   export CORS_ALLOWED_ORIGINS
 else
     bashio::log.error "\"origin\" not set. Please set \"origin\" in add-on config. Aborting."
     exit 1;
 fi
 
 if bashio::config.has_value 'webauthnRpId'; then
-   export WEBAUTHN_RP_ID=$(bashio::config 'webauthnRpId')
+   WEBAUTHN_RP_ID=$(bashio::config 'webauthnRpId')
+   export WEBAUTHN_RP_ID
 else
     bashio::log.error "\"webauthnRpId\" not set. Please set \"webauthnRpId\" in add-on config. Aborting."
     exit 1;
 fi
 
 if bashio::config.true 'registrationEnabled'; then
-   export REGISTRATION_ENABLED=true
+   REGISTRATION_ENABLED=true
+   export REGISTRATION_ENABLED
 else
-   export REGISTRATION_ENABLED=false
+   REGISTRATION_ENABLED=false
+   export REGISTRATION_ENABLED
 fi
 
-export NODE_ENV="production"
+NODE_ENV="production"
+export NODE_ENV
 
 # Start
 bashio::log.info "Starting todo ..."
